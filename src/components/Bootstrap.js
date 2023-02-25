ee9c3ea97634be983d53c5cdbf2f77ade96df95f
bhavanijava <mecsworldb@gmail.com>
2023-02-25 07:03:41 +0530

    first component using bootstrap

---
+++ src/components/Bootstrap.js
@@ -0,0 +1,11 @@
+const Bootstrap=()=>{
+    return(
+        <>
+            <h1 className="text text-success">ReactJs</h1>
+            <h2 className="text text-danger">NodeJs</h2>
+            <h3 className="text text-warning">HTML</h3>
+            <button className="btn btn-primary">Enroll</button>
+        </>
+    )
+};
+export default Bootstrap;
